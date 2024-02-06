352cb2998547e7e6b89639e22dc31f6b5c422442
Sergey-Okey <sergeyborisov_1@vk.com>
2024-02-06 09:05:03 +0300

    update

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-const resultElement = document.getElementById('result')
-const buttonSum = document.getElementById('summa')
-const buttonMinus = document.getElementById('minus')
-const number = document.getElementById('number')
-const number2 = document.getElementById('number2')
-
+const resultElement = document.getElementById("result");
+const buttonSum = document.getElementById("summa");
+const buttonMinus = document.getElementById("minus");
+const number = document.getElementById("number");
+const number2 = document.getElementById("number2");
 
 function printResolve(result) {
 	if (result > 0) {
-		resultElement.style.color = ('green')
+		resultElement.style.color = "green";
 	} else {
-		resultElement.style.color = ('red')
+		resultElement.style.color = "red";
 	}
-	resultElement.textContent = result
+	resultElement.textContent = result;
 }
 
 function computAction(numb1, numb2, actionSymbol) {
-	const clock1 = Number(numb1.value)
-	const clock2 = Number(numb2.value)
-	if (actionSymbol == '+') {
-		return (
-			numb1 + numb2
-		)
+	const clock1 = Number(numb1.value);
+	const clock2 = Number(numb2.value);
+	if (actionSymbol == "+") {
+		return numb1 + numb2;
 	}
-	if (actionSymbol == '-') {
-		return (
-			numb1 - numb2
-		)
-
+	if (actionSymbol == "-") {
+		return numb1 - numb2;
 	}
-
 }
 
 buttonSum.onclick = function () {
-	const sum = Number(number.value) + Number(number2.value)
-	resultElement.textContent = sum
+	const sum = Number(number.value) + Number(number2.value);
+	resultElement.textContent = sum;
 
-	printResolve(sum)
-}
+	printResolve(sum);
+};
 buttonMinus.onclick = function () {
-	const sum = Number(number.value) - Number(number2.value)
-	resultElement.textContent = sum
+	const sum = Number(number.value) - Number(number2.value);
+	resultElement.textContent = sum;
 
-	printResolve(sum)
-}
-
+	printResolve(sum);
+};
